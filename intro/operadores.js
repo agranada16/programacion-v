c468c9bbc4bcf06f400454a6ac5c09aa245f986f
agranada <a.granada16@ciaf.edu.co>
2024-03-14 21:06:30 -0500

    cambios

---
+++ intro/operadores.js
@@ -0,0 +1,38 @@
+//operadores aritmeticos basicos
+let counter = 100;
+
+//1.suma
+counter = counter + 20;
+console.log(counter);
+counter += 20;
+console.log(counter);
+
+//2.resta
+let counter1 = 20;
+
+counter1 = counter1 - 10;
+console.log(counter1);
+counter1 -= 10;
+console.log(counter1);
+
+//3.multiplicacion
+let counter2 = 35;
+
+counter2 = counter2*50;
+console.log(counter2);
+counter2 *= 50;
+console.log(counter2);
+
+//4.division
+let counter3 = 14;
+
+counter3 = counter3/2;
+console.log(counter3);
+counter3 /= 2;
+console.log(counter3)
+
+
+
+
+
+
